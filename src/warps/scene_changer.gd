9986fdbeb8a917546eb33033bdac99cf9e1bbc5a
Rain Yohe <rain.yohe@roushtech.net>
2025-04-19 15:37:40 -0400

    WIP music and preloads

--- src/warps/scene_changer.gd
+++ src/warps/scene_changer.gd
@@ -6,7 +6,9 @@ class_name Warp extends Node2D;
 var warping: bool = false;
 
 func _ready():
+	ResourceLoader.load_threaded_request(level_scene_file);
 	await get_parent().ready;
+	
 	assert(level_scene_file != null,
 		"DoorTrigger '" + name +"' error: No level file provided. Did you forget to set it?");
 	
@@ -21,7 +23,8 @@ func _process(_delta: float) -> void:
 	if (warping):
 		GameDataManager.toworld = level_scene_file;
 		GameDataManager.entry_point = entry_point;
-		get_tree().change_scene_to_file(level_scene_file);
+		var scene = ResourceLoader.load_threaded_get(level_scene_file);
+		get_tree().change_scene_to_file(scene);
 
 
 func waep() -> void:
