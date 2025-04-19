43d6e80bceabffeeea1e9face78c4c4e460b3b9f
Rain Yohe <zty8212000@gmail.com>
2025-04-19 16:09:38 -0400

    Fixes preloading and autoplays music

--- src/warps/scene_changer.gd
+++ src/warps/scene_changer.gd
@@ -6,8 +6,9 @@ class_name Warp extends Node2D;
 var warping: bool = false;
 
 func _ready():
-	ResourceLoader.load_threaded_request(level_scene_file);
 	await get_parent().ready;
+	ResourceLoader.load_threaded_request(level_scene_file, "PackedScene");
+	var status = ResourceLoader.load_threaded_get_status(level_scene_file);
 	
 	assert(level_scene_file != null,
 		"DoorTrigger '" + name +"' error: No level file provided. Did you forget to set it?");
@@ -24,7 +25,8 @@ func _process(_delta: float) -> void:
 		GameDataManager.toworld = level_scene_file;
 		GameDataManager.entry_point = entry_point;
 		var scene = ResourceLoader.load_threaded_get(level_scene_file);
-		get_tree().change_scene_to_file(scene);
+		var status = ResourceLoader.load_threaded_get_status(level_scene_file);
+		get_tree().change_scene_to_packed(scene);
 
 
 func waep() -> void:
